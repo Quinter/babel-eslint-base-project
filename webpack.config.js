4a25069b768099ce395cb44c595b5e12e05855b4
Andrew Quinn <andrew@gallerymediagroup.com>
2019-02-26 12:55:23 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/*eslint-disable no-undef*/
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['eslint-loader']
+			}
+		]
+	},
+};
